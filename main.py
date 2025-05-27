1c7e1e4f7a16bf39212fa98fa0e331d7408595f1
Sajal Sangal <sajalsangal@gmail.com>
2025-05-28 00:08:41 +0530

    Started working on virtual mouse

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import cv2
 import mediapipe
 import time
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+    if not success:
+        break
+
+    flip_img = cv2.flip(img,1)
