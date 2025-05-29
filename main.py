27b497ba00ac1b871b452d7b0b71c1c783647305
Sajal Sangal <sajalsangal@gmail.com>
2025-05-29 22:53:04 +0530

    Added hand recognition feature without using predesigned class

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2
-import mediapipe
+import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
@@ -10,3 +13,21 @@ while True:
         break
 
     flip_img = cv2.flip(img,1)
+    rgbImg = cv2.cvtColor(flip_img, cv2.COLOR_BGR2RGB)
+    result = hands.process(rgbImg)
+
+    if result.multi_hand_landmarks:
+        for handLms in result.multi_hand_landmarks:
+            for id,lms in enumerate(handLms.landmark):
+                h,w,c = img.shape
+                cx,cy = lms.x * h , lms.y * w
+                print([id,cx,cy])
+
+            mpDraw.draw_landmarks(flip_img,handLms,mphands.HAND_CONNECTIONS)
+
+
+    cv2.imshow("Image", flip_img)
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
